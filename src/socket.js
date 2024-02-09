f365f645f0bb359be6423148e9ad73946bd791cc
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-02-09 21:22:29 +0530

    production socket issue

--- src/socket.js
+++ src/socket.js
@@ -1,22 +1,25 @@
 // socket.js
-import { io } from 'socket.io-client';
-
+import { io } from "socket.io-client";
+const options = {
+  withCredentials: true,
+  "force new connection": true,
+  reconnectionAttempt: "Infinity",
+  timeout: 1000,
+  transports: ["websocket"],
+};
+const socket = io(process.env.REACT_APP_BACKEND_URL, options);
 export const initSocket = async () => {
-  const options = {
-    withCredentials: true,
-    'force new connection': true,
-    reconnectionAttempt: 'Infinity',
-    timeout: 1000,
-    transports: ['websocket'],
-  };
-
-  try {
-    const socket = io(`${process.env.REACT_APP_BACKEND_URL}`, options);
-    await socket.connect();
-    console.log('Socket connected with the id:', socket.id);
-    return socket;
-  } catch (error) {
-    console.error('Error connecting to socket:', error.message);
-    throw error;
-  }
+  return socket;
+  //   export const initSocket = () => {
+  //     return socket;
+  // };
+  // try {
+  //   const socket = io(`${process.env.REACT_APP_BACKEND_URL}`, options);
+  //   await socket.connect();
+  //   console.log('Socket connected with the id:', socket.id);
+  //   return socket;
+  // } catch (error) {
+  //   console.error('Error connecting to socket:', error.message);
+  //   throw error;
+  // }
 };
