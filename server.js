4ebb5d5a8f16a04e5449a21d5b45bff3d6b64d38
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-02-09 22:58:54 +0530

    fix: keep alive

--- server.js
+++ server.js
@@ -142,7 +142,9 @@ function getAllConnectedClients(roomId) {
 
 io.on("connection", (socket) => {
   console.log("socket connected", socket.id);
-
+  socket.on("keep-alive", () => {
+    console.log("Received keep-alive message from client:", socket.id);
+  });
   socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
     userSocketMap[socket.id] = username;
     socket.join(roomId);
--- src/socket.js
+++ src/socket.js
@@ -1,22 +1,41 @@
 // socket.js
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-export const initSocket = async () => {
-  const options = {
-    withCredentials: true,
-    'force new connection': true,
-    reconnectionAttempt: Infinity,
-    timeout: 1000,
-    transports: ['websocket'],
-  };
-
-  try {
-    const socket = io(`${process.env.REACT_APP_BACKEND_URL}`, options);
-    await socket.connect();
-    console.log('Socket connected with the id:', socket.id);
-    return socket;
-  } catch (error) {
-    console.error('Error connecting to socket:', error.message);
-    throw error;
-  }
+const options = {
+  withCredentials: true,
+  "force new connection": true,
+  reconnectionAttempt: "Infinity",
+  timeout: 1000,
+  transports: ["websocket"],
 };
+
+const socket = io("http://localhost:5050", options);
+const sendKeepAlive = () => {
+  socket.emit("keep-alive");
+};
+
+setInterval(sendKeepAlive, 30000);
+// Function to initialize the socket
+export const initSocket = () => {
+  return socket;
+};
+
+// export const initSocket = async () => {
+//   const options = {
+//     withCredentials: true,
+//     'force new connection': true,
+//     reconnectionAttempt: Infinity,
+//     timeout: 1000,
+//     transports: ['websocket'],
+//   };
+
+//   try {
+//     const socket = io(`${process.env.REACT_APP_BACKEND_URL}`, options);
+//     await socket.connect();
+//     console.log('Socket connected with the id:', socket.id);
+//     return socket;
+//   } catch (error) {
+//     console.error('Error connecting to socket:', error.message);
+//     throw error;
+//   }
+// };
